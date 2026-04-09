4060a806e13590e163842782e22a10c29b53c2b4
Ulugbek Tojikulov <utojikulovv@gmail.com>
2026-04-09 18:16:16 +0500

    add(market): invoices & payments

--- src/market/market.ts
+++ src/market/market.ts
@@ -924,10 +924,7 @@ export class MarketAPI {
 
 	async editMarketSettings(body?: OpBody<'Profile.Edit'>) {
 		return this.http.withRateLimit(async () => {
-			return await this.http.market.PUT(
-				'/me',
-				body === undefined ? {} : { body },
-			)
+			return await this.http.market.PUT('/me', body === undefined ? {} : { body })
 		})
 	}
 
@@ -936,6 +933,131 @@ export class MarketAPI {
 			return await this.http.market.GET(
 				'/claims',
 				query === undefined ? {} : { params: { query } },
+			)
+		})
+	}
+
+	/** invoices */
+	async getInvoice(query?: OpQuery<'Payments.Invoice.Get'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.GET(
+				'/invoice',
+				query === undefined ? {} : { params: { query } },
+			)
+		})
+	}
+
+	async createInvoice(body?: OpBody<'Payments.Invoice.Create'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.POST(
+				'/invoice',
+				body === undefined ? {} : { body },
+			)
+		})
+	}
+
+	async listInvoices(query?: OpQuery<'Payments.Invoice.List'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.GET(
+				'/invoice/list',
+				query === undefined ? {} : { params: { query } },
+			)
+		})
+	}
+
+	/** payments */
+	async getCurrency() {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.GET('/currency')
+		})
+	}
+
+	async getListOfBalances() {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.GET('/balance/exchange')
+		})
+	}
+
+	async exchangeBalance(body?: OpBody<'Payments.BalanceExchange'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.POST(
+				'/balance/exchange',
+				body === undefined ? {} : { body },
+			)
+		})
+	}
+
+	async transferMoney(body?: OpBody<'Payments.Transfer'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.POST(
+				'/balance/transfer',
+				body === undefined ? {} : { body },
+			)
+		})
+	}
+
+	async checkTransferFee(query?: OpQuery<'Payments.Fee'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.GET(
+				'/balance/transfer/fee',
+				query === undefined ? {} : { params: { query } },
+			)
+		})
+	}
+
+	async cancelTransfer(body?: OpBody<'Payments.Cancel'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.POST(
+				'/balance/transfer/cancel',
+				body === undefined ? {} : { body },
+			)
+		})
+	}
+
+	async getPaymentsHistory(query?: OpQuery<'Payments.History'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.GET(
+				'/user/payments',
+				query === undefined ? {} : { params: { query } },
+			)
+		})
+	}
+
+	async getAutoPayments() {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.GET('/auto-payments')
+		})
+	}
+
+	async createAutoPayment(body?: OpBody<'AutoPayments.Create'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.POST(
+				'/auto-payment',
+				body === undefined ? {} : { body },
+			)
+		})
+	}
+
+	async deleteAutoPayment(body?: OpBody<'AutoPayments.Delete'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.DELETE(
+				'/auto-payment',
+				body === undefined ? {} : { body },
+			)
+		})
+	}
+
+	async getPayoutServices() {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.GET('/balance/payout/services')
+		})
+	}
+
+	async createPayout(body?: OpBody<'Payments.Payout'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.POST(
+				'/balance/payout',
+				body === undefined ? {} : { body },
 			)
 		})
 	}
