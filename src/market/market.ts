ee5c68e710d9f433be4beabd47ab5dae7f8e25a5
Ulugbek Tojikulov <utojikulovv@gmail.com>
2026-04-09 16:00:27 +0500

    add(category): category search methods for all types of items

--- src/market/market.ts
+++ src/market/market.ts
@@ -33,6 +33,102 @@ export class MarketAPI {
 		})
 	}
 
+	async searchSteam(query?: CategoryQuery<'steam'>) {
+		return this.searchCategory('steam', query)
+	}
+
+	async searchTelegram(query?: CategoryQuery<'telegram'>) {
+		return this.searchCategory('telegram', query)
+	}
+
+	async searchFortnite(query?: CategoryQuery<'fortnite'>) {
+		return this.searchCategory('fortnite', query)
+	}
+
+	async searchMihoyo(query?: CategoryQuery<'mihoyo'>) {
+		return this.searchCategory('mihoyo', query)
+	}
+
+	async searchRiot(query?: CategoryQuery<'riot'>) {
+		return this.searchCategory('riot', query)
+	}
+
+	async searchSupercell(query?: CategoryQuery<'supercell'>) {
+		return this.searchCategory('supercell', query)
+	}
+
+	async searchEA(query?: CategoryQuery<'ea'>) {
+		return this.searchCategory('ea', query)
+	}
+
+	async searchWot(query?: CategoryQuery<'wot'>) {
+		return this.searchCategory('wot', query)
+	}
+
+	async searchWotBlitz(query?: CategoryQuery<'wotBlitz'>) {
+		return this.searchCategory('wotBlitz', query)
+	}
+
+	async searchGifts(query?: CategoryQuery<'gifts'>) {
+		return this.searchCategory('gifts', query)
+	}
+
+	async searchEpicGames(query?: CategoryQuery<'epicGames'>) {
+		return this.searchCategory('epicGames', query)
+	}
+
+	async searchEscapeFromTarkov(query?: CategoryQuery<'escapeFromTarkov'>) {
+		return this.searchCategory('escapeFromTarkov', query)
+	}
+
+	async searchSocialClub(query?: CategoryQuery<'socialClub'>) {
+		return this.searchCategory('socialClub', query)
+	}
+
+	async searchUplay(query?: CategoryQuery<'uplay'>) {
+		return this.searchCategory('uplay', query)
+	}
+
+	async searchDiscord(query?: CategoryQuery<'discord'>) {
+		return this.searchCategory('discord', query)
+	}
+
+	async searchTiktok(query?: CategoryQuery<'tiktok'>) {
+		return this.searchCategory('tiktok', query)
+	}
+
+	async searchInstagram(query?: CategoryQuery<'instagram'>) {
+		return this.searchCategory('instagram', query)
+	}
+
+	async searchBattleNet(query?: CategoryQuery<'battleNet'>) {
+		return this.searchCategory('battleNet', query)
+	}
+
+	async searchChatGPT(query?: CategoryQuery<'chatgpt'>) {
+		return this.searchCategory('chatgpt', query)
+	}
+
+	async searchVpn(query?: CategoryQuery<'vpn'>) {
+		return this.searchCategory('vpn', query)
+	}
+
+	async searchRoblox(query?: CategoryQuery<'roblox'>) {
+		return this.searchCategory('roblox', query)
+	}
+
+	async searchWarface(query?: CategoryQuery<'warface'>) {
+		return this.searchCategory('warface', query)
+	}
+
+	async searchMinecraft(query?: CategoryQuery<'minecraft'>) {
+		return this.searchCategory('minecraft', query)
+	}
+
+	async searchHytale(query?: CategoryQuery<'hytale'>) {
+		return this.searchCategory('hytale', query)
+	}
+
 	// More convenience methods can be added here
 	// But users can also use client.market.raw.GET/POST/etc for full access
 }
