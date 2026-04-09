5d31f2f302af05beed6c83a6d4e0afe258e8722b
Ulugbek Tojikulov <utojikulovv@gmail.com>
2026-04-09 18:14:00 +0500

    add(market): account publishing

--- src/market/market.ts
+++ src/market/market.ts
@@ -865,6 +865,53 @@ export class MarketAPI {
 		})
 	}
 
+	/** publishing */
+	async fastAccountUpload(body?: OpBody<'Publishing.FastSell'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.POST(
+				'/item/fast-sell',
+				body === undefined ? {} : { body },
+			)
+		})
+	}
+
+	async addAccount(body?: OpBody<'Publishing.Add'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.POST(
+				'/item/add',
+				body === undefined ? {} : { body },
+			)
+		})
+	}
+
+	async checkAccountDetails(
+		item_id: OpPath<'Publishing.Check'>['item_id'],
+		body?: OpBody<'Publishing.Check'>,
+	) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.POST(
+				'/{item_id}/goods/check',
+				body === undefined
+					? { params: { path: { item_id } } }
+					: { params: { path: { item_id } }, body },
+			)
+		})
+	}
+
+	async addExternalAccount(
+		item_id: OpPath<'Publishing.External'>['item_id'],
+		body?: OpBody<'Publishing.External'>,
+	) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.POST(
+				'/{item_id}/external-account',
+				body === undefined
+					? { params: { path: { item_id } } }
+					: { params: { path: { item_id } }, body },
+			)
+		})
+	}
+
 	// Steam-specific managing methods intentionally skipped for now.
 
 	// More convenience methods can be added here
