5b4e2ad606d6f50b40f0ab1634b422f4ec2e1843
Ulugbek Tojikulov <utojikulovv@gmail.com>
2026-04-09 18:14:47 +0500

    add(market): profile

--- src/market/market.ts
+++ src/market/market.ts
@@ -912,6 +912,34 @@ export class MarketAPI {
 		})
 	}
 
+	/** profile */
+	async getProfile(query?: OpQuery<'Profile.Get'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.GET(
+				'/me',
+				query === undefined ? {} : { params: { query } },
+			)
+		})
+	}
+
+	async editMarketSettings(body?: OpBody<'Profile.Edit'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.PUT(
+				'/me',
+				body === undefined ? {} : { body },
+			)
+		})
+	}
+
+	async getClaims(query?: OpQuery<'Profile.Claims'>) {
+		return this.http.withRateLimit(async () => {
+			return await this.http.market.GET(
+				'/claims',
+				query === undefined ? {} : { params: { query } },
+			)
+		})
+	}
+
 	// Steam-specific managing methods intentionally skipped for now.
 
 	// More convenience methods can be added here
